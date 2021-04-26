fd4e557b2322e4d809a5a5c5bb97bbeeed1c3363
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-04-26 03:28:02 +0300

    validation messages added.

---
+++ lecture2-oopIntro/src/homework/constants/Validations.java
@@ -0,0 +1,9 @@
+package homework.constants;
+
+public class Validations {
+	public static String limitExceeded = "limit exceeded.";
+	public static String canNotBeNull = "cannot be null";
+	
+	public static String nameLengthExceeded = "character " + limitExceeded;
+	public static String startDateAndEndDateCanNotBeNull = "startDate and endDate " + canNotBeNull;
+}
