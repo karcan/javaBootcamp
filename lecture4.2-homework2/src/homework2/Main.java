a67a12b78c9cee89818d2a341e54cd3319363373
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-02 18:26:00 +0300

    lecture4 homework2 without mernis

--- lecture4.2-homework2/src/homework2/Main.java
+++ lecture4.2-homework2/src/homework2/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		Customer enginCustomer = new Customer();
 		enginCustomer.setId(1);
 		enginCustomer.setFirstName("Engin");
-		enginCustomer.setLastName("Duran");
+		enginCustomer.setLastName("Demiroğ");
 		enginCustomer.setBirthDate(new GregorianCalendar(1986, 5 , 1).getTime());
 		
 		CustomerService customerService = new StarbucksCustomerManager(new BaseCustomerCheckManager());
