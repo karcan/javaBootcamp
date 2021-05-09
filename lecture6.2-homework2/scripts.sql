c5561c5c0456d094398f7b66da61ec8f6bba1283
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-09 21:16:44 +0300

    relations little changes

--- lecture6.2-homework2/scripts.sql
+++ lecture6.2-homework2/scripts.sql
@@ -5,12 +5,6 @@ DROP TABLE IF EXISTS companies;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS job_titles;
-DROP TABLE IF EXISTS admins;
-
-CREATE TABLE "public"."admins"( 
-	"id" int NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-	"username" varchar(25),
-	"password" varchar(25));
 
 CREATE TABLE "public"."candidates"( 
 	"user_id" int NOT NULL,
@@ -24,19 +18,19 @@ CREATE TABLE "public"."companies"(
 	"company_name" varchar(255) NOT NULL,
 	"web_address" varchar(50) NOT NULL,
 	"phone_number" varchar(12) NOT NULL,
-	"is_active" boolean NOT NULL);
+	"is_activated" boolean NOT NULL);
 
 CREATE TABLE "public"."company_activation_codes"( 
-	"id" int NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-	"company_id" int NOT NULL,
+	"id" int NOT NULL,
+	"company_user_id" int NOT NULL,
 	"activation_code" varchar(38) NOT NULL,
 	"is_confirmed" boolean NOT NULL,
 	"confirmed_date" date);
 
 CREATE TABLE "public"."company_activation_employees"( 
-	"id" int NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-	"company_id" int NOT NULL,
-	"confirmed_employee_id" int,
+	"id" int NOT NULL,
+	"company_user_id" int NOT NULL,
+	"confirmed_employee_user_id" int,
 	"confirmed_date" date);
 
 CREATE TABLE "public"."employees"( 
@@ -45,17 +39,20 @@ CREATE TABLE "public"."employees"(
 	"last_name" varchar(25) NOT NULL);
 
 CREATE TABLE "public"."job_titles"( 
-	"id" int NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+	"id" int NOT NULL,
 	"title" varchar(50) NOT NULL);
 
 CREATE TABLE "public"."users"( 
-	"id" int NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+	"id" int NOT NULL,
 	"email" varchar(50) NOT NULL,
 	"password" varchar(25) NOT NULL);
 	
+CREATE SEQUENCE "public"."company_activation_codes_id_seq" INCREMENT BY 1 START WITH 1 OWNED BY "public"."company_activation_codes"."id";
+CREATE SEQUENCE "public"."company_activation_employees_id_seq" INCREMENT BY 1 START WITH 1 OWNED BY "public"."company_activation_employees"."id";
+CREATE SEQUENCE "public"."job_titles_id_seq" INCREMENT BY 1 START WITH 1 OWNED BY "public"."job_titles"."id";
+CREATE SEQUENCE "public"."users_id_seq" INCREMENT BY 1 START WITH 1 OWNED BY "public"."users"."id";
 ALTER TABLE "public"."admins" ADD CONSTRAINT "pk_admin" PRIMARY KEY ("id");
 ALTER TABLE "public"."candidates" ADD CONSTRAINT "pk_candidate" PRIMARY KEY ("user_id");
-ALTER TABLE "public"."candidates" ADD CONSTRAINT "uc_identity_numbers" UNIQUE ("identity_number");
 ALTER TABLE "public"."companies" ADD CONSTRAINT "pk_company" PRIMARY KEY ("user_id");
 ALTER TABLE "public"."company_activation_codes" ADD CONSTRAINT "pk_companyactivationcode" PRIMARY KEY ("id");
 ALTER TABLE "public"."company_activation_employees" ADD CONSTRAINT "pk_companyactivationemployee" PRIMARY KEY ("id");
@@ -63,9 +60,14 @@ ALTER TABLE "public"."employees" ADD CONSTRAINT "pk_employee" PRIMARY KEY ("user
 ALTER TABLE "public"."job_titles" ADD CONSTRAINT "pk_job_titles" PRIMARY KEY ("id");
 ALTER TABLE "public"."users" ADD CONSTRAINT "pk_user" PRIMARY KEY ("id");
 ALTER TABLE "public"."job_titles" ADD CONSTRAINT "uc_job_titles" UNIQUE ("title");
-ALTER TABLE "public"."company_activation_codes" ADD CONSTRAINT "fk_companyactivationcode_company" FOREIGN KEY ("company_id") REFERENCES "public"."companies" ( "user_id");
-ALTER TABLE "public"."company_activation_employees" ADD CONSTRAINT "fk_companyactivationemployee_company" FOREIGN KEY ("company_id") REFERENCES "public"."companies" ( "user_id");
-ALTER TABLE "public"."users" ADD CONSTRAINT "fk_user_candidate" FOREIGN KEY ("id") REFERENCES "public"."candidates" ( "user_id");
-ALTER TABLE "public"."users" ADD CONSTRAINT "fk_user_company" FOREIGN KEY ("id") REFERENCES "public"."companies" ( "user_id");
-ALTER TABLE "public"."users" ADD CONSTRAINT "fk_user_employee" FOREIGN KEY ("id") REFERENCES "public"."employees" ( "user_id");
-ALTER TABLE "public"."companies" ALTER COLUMN "is_active" SET DEFAULT false;
+ALTER TABLE "public"."candidates" ADD CONSTRAINT "fk_candidates_users" FOREIGN KEY ("user_id") REFERENCES "public"."users" ( "id");
+ALTER TABLE "public"."companies" ADD CONSTRAINT "fk_companies_users" FOREIGN KEY ("user_id") REFERENCES "public"."users" ( "id");
+ALTER TABLE "public"."company_activation_codes" ADD CONSTRAINT "fk_company_activation_codes_companies" FOREIGN KEY ("company_user_id") REFERENCES "public"."companies" ( "user_id");
+ALTER TABLE "public"."company_activation_employees" ADD CONSTRAINT "fk_company_activation_employees_companies" FOREIGN KEY ("company_user_id") REFERENCES "public"."companies" ( "user_id");
+ALTER TABLE "public"."company_activation_employees" ADD CONSTRAINT "fk_company_activation_employees_employees1" FOREIGN KEY ("confirmed_employee_user_id") REFERENCES "public"."employees" ( "user_id");
+ALTER TABLE "public"."employees" ADD CONSTRAINT "fk_employees_users" FOREIGN KEY ("user_id") REFERENCES "public"."users" ( "id");
+ALTER TABLE "public"."companies" ALTER COLUMN "is_activated" SET DEFAULT false;
+ALTER TABLE "public"."company_activation_codes" ALTER COLUMN "id" SET DEFAULT nextval('"public"."company_activation_codes_id_seq"');
+ALTER TABLE "public"."company_activation_employees" ALTER COLUMN "id" SET DEFAULT nextval('"public"."company_activation_employees_id_seq"');
+ALTER TABLE "public"."job_titles" ALTER COLUMN "id" SET DEFAULT nextval('"public"."job_titles_id_seq"');
+ALTER TABLE "public"."users" ALTER COLUMN "id" SET DEFAULT nextval('"public"."users_id_seq"');
