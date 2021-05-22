e2ff4b1dabc5988caad6ed00424da26eccd0405b
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-22 23:52:05 +0300

    .

--- lecture7.1-swaggerAndResult/src/main/java/kodlamaio/northwind/entity/concretes/Product.java
+++ lecture7.1-swaggerAndResult/src/main/java/kodlamaio/northwind/entity/concretes/Product.java
@@ -7,11 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
