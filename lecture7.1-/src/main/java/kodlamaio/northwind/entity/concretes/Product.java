f4fca4108c665b59c9e254b9096e56d42403a486
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-22 21:46:56 +0300

    added lecture 7.1 swagger

---
+++ lecture7.1-/src/main/java/kodlamaio/northwind/entity/concretes/Product.java
@@ -0,0 +1,34 @@
+package kodlamaio.northwind.entity.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="products")
+public class Product {
+	@Id
+	@GeneratedValue
+	@Column(name="product_id")
+	private int id;
+	
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	@Column(name="unit_price")
+	private double unitPrice;
+	
+	@Column(name="units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name="quantity_per_unit")
+	private String quantityPerUnit;
+}
